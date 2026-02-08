6c963de99a9625d8f653ad327d164f1e60dcf54e
Abhit Raj <abhitraj14@gmail.com>
2026-02-08 19:51:46 +0530

    Time: 0 ms (100%), Space: 12.6 MB (0%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        
+        while num >= 10:
+            s = 0
+            while num > 0:
+                s += num%10
+                num //= 10
+
+            num = s
+        return num
