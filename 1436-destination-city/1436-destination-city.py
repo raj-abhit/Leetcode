36f69f27a684cb3156fd81ac69b74102c5c50259
Abhit Raj <abhitraj14@gmail.com>
2026-03-19 00:00:36 +0530

    Time: 0 ms (100%), Space: 12.5 MB (49.09%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def destCity(self, paths):
+        """
+        :type paths: List[List[str]]
+        :rtype: str
+        """
+        sources = set()
+        for src,dst in paths:
+            sources.add(src)
+
+        for src, dst in paths:
+            if dst not in sources:
+                return dst
+        
