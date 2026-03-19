821fa36be9cef63355964d76c946f4df025a38ff
Abhit Raj <abhitraj14@gmail.com>
2026-03-19 17:34:07 +0530

    Time: 451 ms (30.47%), Space: 12.2 MB (96.74%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        row =  1
+        while n>=row:
+            n -= row
+            row +=1
+        return row -1
+            
