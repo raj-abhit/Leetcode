d4a2fdeecd826649f32c60f7ed8322cca2c9b166
Abhit Raj <abhitraj14@gmail.com>
2026-05-19 18:45:28 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.39%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+        count0 = students.count(0)
+        count1 = students.count(1)
+
+        for sand in sandwiches:
+
+            if sand == 0:
+                if count0 == 0:
+                    return count1
+                count0 -= 1
+
+            else:
+                if count1 == 0:
+                    return count0
+                count1 -= 1
+
+        return 0
+            
